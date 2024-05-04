0cd672588f8d5fea90dea85b65c9fa506db5845b
THG20203 <tristanhgriffiths8@yahoo.com>
2024-05-04 18:39:28 +0100

    including images in header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "../../assets/styles/Header/header.scss";
+/* Header Images */
+import mainHeader from "../../assets/images/main-header.png";
+import secondaryHeaderImage from "../../assets/images/header-second.png";
+import whatCanImage from "../../assets/images/what-can-we-do.png";
+import whyChooseImage from "../../assets/images/why-choose-us.png";
 
 const Header = () => {
   return (
     <div className="header__container">
-      <div>test</div>
+      <img className="header__image" src={mainHeader} />
+      <img className="header__image" src={secondaryHeaderImage} />
+      <img className="header__image" src={whatCanImage} />
+      <img className="header__image" src={whyChooseImage} />
     </div>
   );
 };
