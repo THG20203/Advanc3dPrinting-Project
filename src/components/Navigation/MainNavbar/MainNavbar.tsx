f545b4d10dfb2713ae0f389dd4d987471b17f4a6
THG20203 <tristanhgriffiths8@yahoo.com>
2024-05-04 23:51:29 +0100

    continuing mainavbar

--- src/components/Navigation/MainNavbar/MainNavbar.tsx
+++ src/components/Navigation/MainNavbar/MainNavbar.tsx
@@ -1,37 +1,87 @@
 import React, { useState } from "react";
 import "../../../assets/styles/Navigation/main-navbar.scss";
 import { NavLink } from "react-router-dom";
+import mainLogo from "../../../assets/images/main-logo.png";
 
 const MainNavbar = () => {
   return (
     <div className="main-navbar__container">
       <div className="main-navbar__content">
+        <img className="main-navbar__logo" src={mainLogo} />
         <nav>
-          <ul>
-            <li>
+          <ul className="main-navbar__list">
+            <li className="main-navbar__list--item">
               <NavLink
                 to="/"
-                className="nav__link"
+                className="main-navbar__link"
                 /* onClick={handleLinkClick} */
               >
                 Home
               </NavLink>
             </li>
-            <li>Process</li>
-            <li>
+            <li className="main-navbar__list--item">
+              <NavLink
+                to="#"
+                className="main-navbar__link"
+                /* onClick={handleLinkClick} */
+              >
+                Process
+              </NavLink>
+            </li>
+            <li className="main-navbar__list--item">
               <NavLink
                 to="about"
-                className="nav__link"
+                className="main-navbar__link"
                 /* onClick={handleLinkClick} */
               >
                 About
               </NavLink>
             </li>
-            <li>Design Support</li>
-            <li>Manufacturing</li>
-            <li>Material Selection</li>
-            <li>3d Printing</li>
-            <li>Get in Touch</li>
+            <li className="main-navbar__list--item">
+              <NavLink
+                to="#"
+                className="main-navbar__link"
+                /* onClick={handleLinkClick} */
+              >
+                Design Support
+              </NavLink>
+            </li>
+            <li className="main-navbar__list--item">
+              <NavLink
+                to="#"
+                className="main-navbar__link"
+                /* onClick={handleLinkClick} */
+              >
+                Manufacturing
+              </NavLink>
+            </li>
+            <li className="main-navbar__list--item">
+              <NavLink
+                to="#"
+                className="main-navbar__link"
+                /* onClick={handleLinkClick} */
+              >
+                Material Selection
+              </NavLink>
+            </li>
+            <li className="main-navbar__list--item">
+              <NavLink
+                to="#"
+                className="main-navbar__link"
+                /* onClick={handleLinkClick} */
+              >
+                3d Printing
+              </NavLink>
+            </li>
+            <li className="main-navbar__list--item">
+              <NavLink
+                to="#"
+                className="main-navbar__link"
+                /* onClick={handleLinkClick} */
+              >
+                Contact Us
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </div>
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -4,7 +4,7 @@ Depending on what link the user clicks on your website, <Outlet />
 changes to show the appropriate page. For example, if someone clicks 
 on a link to go to the home page, <Outlet /> will show the home page. */
 
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import FirstNavbar from "../components/Navigation/FirstNavbar/FirstNavbar";
 import MainNavbar from "../components/Navigation/MainNavbar/MainNavbar";
 import "../assets/styles/General/root.scss";
