cbe226d0dfa451de61dffdae738e264b05b6d34a
THG20203 <tristanhgriffiths8@yahoo.com>
2024-05-06 23:37:04 +0100

    beginning marquee

---
+++ src/components/Marquee/Marquee.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../assets/styles/Marquee/marquee.scss";
+
+const Marquee = () => {
+  return (
+    <div className="marquee__container">
+      <div className="marquee__content"></div>
+    </div>
+  );
+};
+
+export default Marquee;
