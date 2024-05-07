cc5f5b59674ad2728488bc099328023a1779ad95
THG20203 <tristanhgriffiths8@yahoo.com>
2024-05-07 22:23:35 +0100

    working on contact page

--- src/pages/mainPages/Contact.tsx
+++ src/pages/mainPages/Contact.tsx
@@ -8,14 +8,25 @@ const ContactPage = () => {
       <div className="contact__container">
         <SpeechBubble />
         <h2 className="heading-2 mb-md">Get in Touch</h2>
-        <p className="paragraph-text mb-sm">
+        <p className="paragraph-text">
           Feel free to contact us and discuss your 3D printing needs.
         </p>
         <p className="paragraph-text mb-lg">
           We are here to assist you from concept to production.
         </p>
-        <p className="paragraph-text mb-sm">advanc3dprintingltd@gmail.com</p>
-        <p className="paragraph-text mb-sm">+44 (0)7757 905335</p>
+        <a
+          className="contact__link"
+          href="mailto:advanc3dprintingltd@gmail.com"
+        >
+          <p className="paragraph-text mb-sm contact-bold">
+            advanc3dprintingltd@gmail.com
+          </p>
+        </a>
+        <a className="contact__link" href="t:+447757905335">
+          <p className="paragraph-text mb-md contact-bold">
+            +44 (0)7757 905335
+          </p>
+        </a>
         <p className="paragraph-text mb-xl">
           <span className="address-text">502 Broadway</span>
           <br />
