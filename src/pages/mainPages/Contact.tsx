6dadef1bed2afe8575c0f7d32a28977c3cdf1b0b
THG20203 <tristanhgriffiths8@yahoo.com>
2024-05-07 21:49:08 +0100

    get in touch work

--- src/pages/mainPages/Contact.tsx
+++ src/pages/mainPages/Contact.tsx
@@ -7,6 +7,24 @@ const ContactPage = () => {
     <div className="container">
       <div className="contact__container">
         <SpeechBubble />
+        <h2 className="heading-2 mb-md">Get in Touch</h2>
+        <p className="paragraph-text mb-sm">
+          Feel free to contact us and discuss your 3D printing needs.
+        </p>
+        <p className="paragraph-text mb-lg">
+          We are here to assist you from concept to production.
+        </p>
+        <p className="paragraph-text mb-sm">advanc3dprintingltd@gmail.com</p>
+        <p className="paragraph-text mb-sm">+44 (0)7757 905335</p>
+        <p className="paragraph-text mb-xl">
+          <span className="address-text">502 Broadway</span>
+          <br />
+          <span className="address-text">Letchworth</span>
+          <br />
+          <span className="address-text">SG6 3PT</span>
+          <br />
+          <span className="address-text">UK</span>
+        </p>
       </div>
     </div>
   );
