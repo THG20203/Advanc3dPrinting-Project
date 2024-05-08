61ad7f72442bb30d608863c86fca028a59675e78
THG20203 <tristanhgriffiths8@yahoo.com>
2024-05-08 13:32:09 +0100

    beginning sorting out routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,23 @@ import "./assets/styles/General/utilities.scss";
 /* Router and react-router-dom */
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+/* General Routes / Pages */
 import ErrorPage from "./pages/Error";
 import RootLayout from "./pages/Root";
+
+/* Main Pages */
 import HomePage from "./pages/mainPages/Home";
-import DesignProcessPage from "./pages/mainPages/DesignProcess";
-import ManufacturingProcessPage from "./pages/mainPages/ManufacturingProcess";
 import AboutPage from "./pages/mainPages/About";
-import GalleryPage from "./pages/mainPages/Gallery";
 import ThreeDPrintingPage from "./pages/mainPages/ThreeDPrinting";
 import ContactPage from "./pages/mainPages/Contact";
+
+/* Secondary Pages */
+// Secondary Manufacturing
+import DesignProcessPage from "./pages/secondaryPages/DesignProcess";
+import ManufacturingProcessPage from "./pages/secondaryPages/ManufacturingProcess";
+// Secondary Design Support
+import GalleryPage from "./pages/secondaryPages/Gallery";
+// Secondary Manufacturing
 
 const router = createBrowserRouter([
   {
